35bc5249de47ad5f79b1b5924196684f7404dcc2
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 20:39:16 +0500

    data-type conservation tutorial completed

---
+++ 05_datatype-comparison.js
@@ -0,0 +1,24 @@
+console.log(2==2)
+console.log(2!=2)
+console.log(2!==2)
+console.log(2===2)
+console.log(2>=2)
+console.log(2<=2)
+console.log("2" > 2)|
+
+
+
+console.log(null==0)
+console.log(null!=0)
+console.log(null!==0)
+console.log(null===0)
+console.log(null>=0)
+console.log(null<=0)
+console.log("0">null)
+
+
+console.log("2"==2)
+console.log("2"===2)
+
+
+
