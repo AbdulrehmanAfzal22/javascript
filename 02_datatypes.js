ca0603938d50c06a93891654619f3aee073dbde9
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 19:15:18 +0500

    datatypes tutorial completed

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+// alert(3+3) we are only studying node
+let name = "abdulrehman"
+let age = 33
+let  email = null
+let account;
+let islogin = false
+ console.log(typeof email)
+// Data-types
+// null
+// Number
+// BigInt
+// Symbol
+// String
+// Boolean
+// undefined
