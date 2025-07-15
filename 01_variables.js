f4da0b17723c4174ddbb74338f696d67872b128e
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 18:19:41 +0500

    Variables tutorial completed

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123214
+let email = "abdulrehman@gmail.com"
+var accountpassword = "abdulrehman12"
+accountcity = "lahore"
+accountstate;
+// accountId = 2
+email = "esf@gmail.com"
+accountpassword = "qwerty"
+
+// console.log(accountId)
+ 
+console.table([accountId, email ,accountpassword ,accountcity ] )
