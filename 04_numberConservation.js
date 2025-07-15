ed6e403d7ac5f52794c8d89a9963f7d5f590680d
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 20:26:00 +0500

    number-conservation tutorial completed

---
+++ 04_numberConservation.js
@@ -0,0 +1,23 @@
+let value = 3
+let negvalue = -value
+// console.log (negvalue) 
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2) 
+console.log("1" + 2)
+console.log(1 + "2")
+console.log( 1 + "2" + 2)
+console.log ( 1 + 2 + "3")
+
+
+let str1 = "hello"
+let str2 = "Ali"
+console.log(str1 + str2)
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter)
