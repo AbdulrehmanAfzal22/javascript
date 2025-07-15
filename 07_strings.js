6cd7519d3d9506424c75e58e1de29d9404d9ca69
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-16 00:25:40 +0500

    strings tutorial completed

---
+++ 07_strings.js
@@ -0,0 +1,43 @@
+const name = "ali"
+
+const repoCount= 25 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}` )  
+
+let gameName = new String('ALi')
+
+// console.log(gameName)
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('i'))
+const substring = gameName.substring(0,2)
+console.log(substring)
+let a = "Atom"
+console.log(a[1])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+// console.log(a[4])
+console.log(a.length)
+
+
+let name1 = "harry"
+let friend = "ali"
+console.log("Myn name is"+ name1 + "and my friend name is " + friend)
+ console.log(`My name is ${name1} and my friend name is ${friend}`)
+
+
+let b ="anees"
+ console.log(b.toUpperCase())
+ console.log(b.toLowerCase())
+ console.log(b.length)
+ console.log(b.slice(1))
+ console.log(b.slice(3))
+ console.log(b.replace("an",66))
+ console.log(b.concat(a, "Ali", name1))
+ console.log(b.trim)
+ console.log(b.startsWith("an"))
+ console.log(b.endsWith("an"))
+ console.log(b.indexOf("an"))
