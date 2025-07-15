0bdcb13e3c3f788eba270c0fcb3168054ef7f6b7
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 20:45:57 +0500

    data-type primitive/non-primitive tutorial co\mpleted

---
+++ 06_datatype-summary.js
@@ -0,0 +1,18 @@
+let a = null;
+let b = 345;
+let c = true; 
+let d = BigInt("567") + BigInt("3")
+let e = "ali"
+let f = Symbol("I am a nice symbol")
+let g
+console.log(a, b, c , d, e, f, g)
+console.log(typeof c)
+
+
+const item = {
+  "Adeel": true,
+  "Ali": false,
+  "Ashan": 67,
+  "Ramish": undefined
+}
+console.log(item["Ali"])
