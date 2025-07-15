d9bae8d3848aac14d864e299b5edf37731b3c5cf
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 20:56:50 +0500

    data-type primitive/non-primitive and storage  tutorial completed

--- 06_datatype-summary.js
+++ 06_datatype-summary.js
@@ -16,3 +16,22 @@ const item = {
   "Ramish": undefined
 }
 console.log(item["Ali"])
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack (primitive datatype) , heap (non-primitive datatype)
+
+let myname = "Ali"
+let anothername = myname
+anothername = "ramish"
+console.log(anothername)
+
+let user1={
+  email : "user@gmail.com",
+  password : "abc"
+}
+
+let user2 = user1
+user2.email = "google@gmail.com"
+console.log(user1)
+console.log(user2)
