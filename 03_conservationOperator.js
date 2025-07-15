796c7e0f7a05ee6f61f92fd8b2a11d1f13b77189
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-15 20:12:11 +0500

    conservation-operator tutorial completed

---
+++ 03_conservationOperator.js
@@ -0,0 +1,24 @@
+let score = "33aa"
+
+console.log(score)
+
+console.log(typeof score )
+
+let valueinNumber = Number(score)
+
+console.log(typeof valueinNumber )
+
+console.log(  valueinNumber )
+
+let isLoggedIn = 1
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanisLoggedIn)
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
