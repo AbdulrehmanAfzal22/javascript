61f92c9b04c1f7002c8cbeb0d8ba9de3cef6b6ab
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-16 13:26:40 +0500

    math-number tutorial completed

---
+++ 08_math-number.js
@@ -0,0 +1,31 @@
+// const score = 400
+// console.log(score)
+// const balance = new Number(100)
+// console.log
+// (balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(8))
+
+// const othernumber = 123.5435
+// console.log(othernumber.toPrecision(3)) 
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4,4,3,23,54))
+// console.log(Math.max(4,4,3,23,54))
+// console.log(Math.random()*10 + 1)
+
+const min = 10
+const max = 20
+// console.log(Math.random()*(max - min + 1) + min);
+console.log(Math.floor(Math.random()*(max - min + 1) + min));
+
