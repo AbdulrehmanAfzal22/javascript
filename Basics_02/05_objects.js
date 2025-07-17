3ed9f61a5b4b2aa9eb87be59be230f6ade74949c
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-17 13:55:33 +0500

    objects-json  tutorial completed

---
+++ Basics_02/05_objects.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename : "Javascript",
+    price : "999",
+    courseinstructor:"Ali"
+}
+const {courseinstructor:instructor} = course
+console.log(instructor);
+
+
+// {
+//    coursename : "Html",
+//     price : "free",
+//     courseinstructor:"Ali shaihd" 
+// }
