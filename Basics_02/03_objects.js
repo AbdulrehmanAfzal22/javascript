a0aed9811d980007635fa67f1d84bc2a96ea7dce
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-17 13:11:04 +0500

    objects tutorial completed

---
+++ Basics_02/03_objects.js
@@ -0,0 +1,12 @@
+const user ={
+    name  : "Ali",
+    email : "ali@gmail.com",
+    isLoggedIn : false,
+    location :"Lahore", 
+    lastLoggedInDays: ["Monday","Tuesday","Friday","Sunday"],
+    "Full name":"Abdulrehman afzal"
+}
+
+console.log(user.email)
+console.log(user["email"])
+console.log(user["Full name"])
