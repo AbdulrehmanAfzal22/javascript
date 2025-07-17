edcdcd8d0f8148da74fb80b73e5bf52cf64f6b00
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-17 13:39:39 +0500

    objects-2  tutorial completed

---
+++ Basics_02/04_objects.js
@@ -0,0 +1,28 @@
+// const pubgUser = {};
+
+// pubgUser.name = "Ali";
+// pubgUser.id = 547476;
+// pubgUser.isLoggedIn = false;
+
+// console.log(pubgUser);
+
+const regularUser = {
+  id: 547476,
+  email: "Ali@gmail.com",
+  Fullname: {
+    userFullname: {
+      firstname: "Ali",
+      isLoggedIn: false,
+    },
+  },
+};
+// console.log(Object.keys(regularUser));
+console.log(Object.hasOwnProperty('isLoggedIn'));
+// console.log(Object.values(regularUser));
+// console.log(regularUser.firstname);
+
+// let obj1 = {1:"a", 2:"b", 3:"c" }
+// let obj2 = {4:"a", 5:"b", 6:"c" }
+// let obj3 = {7:"a", 8:"b", 9:"c" }
+// let obj4 = Object.assign(obj1,obj2)
+// console.log(obj4);
