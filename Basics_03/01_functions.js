b97c512017783112d16b3cea1bc410d77bbd120a
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-17 14:32:08 +0500

    functions  tutorial completed

---
+++ Basics_03/01_functions.js
@@ -0,0 +1,30 @@
+// function saMyName(){
+//     console.log("A");
+//     console.log("l");
+//     console.log("i");
+// }
+// saMyName
+
+// function addTwoNumbers(n1,n2) {
+//     console.log(n1 + n2) ;
+// }
+// addTwoNumbers()
+
+// function addtwoNumbers(n1,n2) {
+//     return n1 + n2
+//     let result = console.log(n1 + n2);
+    
+
+// }
+// addTwoNumbers()
+
+ function userLoggedIn (user){
+    if (user === undefined) {
+        console.log("Please enter a value");
+        return
+    }
+    return  `${user} just logged in`
+ }
+// console.log( userLoggedIn("rahim"));
+console.log( userLoggedIn());
+
