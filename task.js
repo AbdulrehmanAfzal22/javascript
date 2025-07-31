90092ac412c63a58aa3a4a4f10ec5e322ca38ebf
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-31 19:26:10 +0500

    Tasks completed / capitalize letter

---
+++ task.js
@@ -0,0 +1,31 @@
+// let input1 = prompt("Enter you age")
+// let input12 = prompt("Enter your age")
+// console.log(`The name of the user ${input1} and his agr is ${input12}`)
+
+
+// Write a function called greetUser that takes a single parameter, name. If the name is empty or only spaces, return "Name cannot be empty." Otherwise, trim the name, capitalize the first letter, and return "Hello, <Name>! Welcome to the platform." Then, prompt the user for their name, call the function, and print the result.
+
+
+
+// let capital;
+// let greetUser = function (name) {
+
+//     if (name2 = "") {
+//         console.log("Name cannot be empty.")
+//     }
+//      capital = name
+  
+//     let str = (capital[0].toUpperCase())
+//        let rest = name.slice(1)
+//      let result = str += rest
+     
+//       console.log(`Hello,${result} Welcome to the platform`)
+  
+  
+// }
+// greetUser("ali")
+
+// let arr = [1,2,3,4]
+// console.log(arr.slice(1))
+// let arr2 = arr
+// console.log(arr)
