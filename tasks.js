5ddfd27876459fec08c52cc7a6cfda9340b7bc28
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-07-31 15:14:06 +0500

    Tasks completed

---
+++ tasks.js
@@ -0,0 +1,109 @@
+// Task:1
+
+// let  name1 = "Abdulrehman"
+// console.log(`Welcome ${name1}`)
+
+
+
+// Task:2
+
+// let num1 = 44
+// let num2  = 33
+// console.log(`The sum of num1 and num2 = ${num1+num2} `);
+// console.log(`The multiplication of num1 and num2 = ${num1*num2} `);
+// console.log(`The  division of num1 and num2 = ${num1/num2} `);
+// console.log(`The modulus of num1 and num2 = ${num1%num2} `);
+// console.log(`The subtraction of num1 and num2 = ${num1-num2} `);
+ 
+
+// Task:3
+
+// let isevenOdd = function (number) {
+//     let number2 = parseInt(number)
+//     if (number % 2 ===0) {
+//         console.log(`This is an even number `);
+//     } else {
+//         console.log(`This is an odd number `);
+//     }
+// }
+// isevenOdd(35)
+
+// Task:4
+// let greater;
+// let maximum = function (num3 , num4 , num5) {
+// if (num3>num4) {
+//     console.log(`${num3}  is greater `);
+//     greater = num3
+// }
+// else if(num4>num5){
+//      console.log(`${num4}  is greater`);
+//      greater = num4
+// }
+// else{
+//    greater = num5
+//     console.log(`${greater}  is greater`);
+// }
+// }
+// maximum(44,55,66)
+
+// Task:5 
+// let reverse = function (string) {
+//     let reversestr = ""
+//     for (let i = string.length -1; i >= 0; i--) {
+
+//         reversestr +=string[i]
+//     }
+    
+//    console.log(`${reversestr}`);
+// }
+// reverse("Abdulrehman")
+
+// Task:6
+
+// let personAge = function (age) {
+//     if (age >= 18) {
+//         console.log("You are eligble for vote");
+        
+//     } else {
+//             console.log("You are not eligble for vote");
+//     }
+// }
+// personAge(34)
+
+// Task:7
+
+// let obj = [
+//     Name = "abdulrehman",
+//     Age = 20,
+//     City = "lahore"
+// ]
+// console.log(`My name is ${Name}, My age is ${Age} and I lived in ${City} `)
+
+// Task:8
+
+
+// let string = function (str) {
+//      let first = str[0].toUpperCase()
+//      let rest = str.slice(1)
+//      let result = first + rest
+//      console.log(result)
+// }
+// string("abdulrehman")
+
+// Task:9
+
+// for (let i = 5; i < 51; i +=5) {
+// console.log(i);
+
+// }
+
+// Task:10
+// let arr = [1,2,3,4,54,8]
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]   
+// }
+// console.log(sum)
+
+// Task:11
+
