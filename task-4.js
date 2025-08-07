31ffbc87297b52b9d423596efd4494f0e3c70359
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-08-07 14:06:28 +0500

    All array  Tasks completed

---
+++ task-4.js
@@ -0,0 +1,95 @@
+// Count how many even numbers are in an array
+
+// let array = [1,34,3,4,4,5,32,54,5,235,32,2]
+// let count = 0
+// for (let i = 0; i < array.length; i++) {
+// if (array[i] % 2 ===0) {
+//     count ++
+// }
+
+// }
+// console.log(count)
+
+// Count how many vowels are in a string (assume all lowercase)
+
+// let word = "Apple"
+// let vowel= 0
+// let lowercase = word.toLowerCase()
+// for (let i = 0; i < word.length; i++) {
+//     if ("aeiou".includes(lowercase[i])) {
+//         vowel++
+//     }
+    
+// }
+// console.log(vowel);
+
+// Check if a string is a palindrome'
+
+
+// let str = prompt("Enter a string:")
+// let lowercase = str.toLowerCase()
+// let reverse = lowercase.split("").reverse().join("")
+// if(reverse == str){
+//     console.log(`${reverse} is an palindrome`)
+// }
+// else{
+//     console.log(`${str} is not a palindrome`)
+// }
+
+
+// Count how many numbers in an array are greater than a given number
+// let input = prompt("Enter your number:")
+// let array = [1,2,3,4,5,6,7,,8,9,10]
+// let count = 0
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] > input) {
+//         count++
+//     }
+    
+// }
+// console.log(count)
+
+
+// Count how many words are in a string (words are separated by a single space)
+
+
+// let word = prompt("Enter your word:")
+// let split = word.split(" ")
+// console.log(split.length)
+
+// Count how many times a specific character appears in a string
+
+
+// let character = prompt("Enter your chracter to see how many in repeated in word")
+// let word = "Banana"
+// let str = word.toLowerCase()
+// let count = 0
+// for (let i = 0; i < str.length; i++) {
+// if (character === str[i]) {
+//     count++
+// }
+
+// }
+// console.log(count);
+
+
+// Find the first non-repeating character in a string
+
+
+// INCOMPLETD
+
+// let str = "abbabrrac"
+// let lowercase = str.toLowerCase()
+// for (let i = 0; i < lowercase.length; i++) {
+//     if (!lowercase[i] === str) {
+//         console.log(lowercase[i])
+//     }
+    
+// }
+
+
+// let array = [1,3,5,45,7,4,45,435,3,6,3]
+// array.sort(function(a,b){
+//     return a-b
+// })
+//     console.log(array)
